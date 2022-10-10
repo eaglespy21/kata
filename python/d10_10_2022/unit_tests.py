cbe1a1b2d56bf1f3c9bbb2c28b78fc171630a995
Saurabh Hindlekar <saurabhhindlekar@hotmail.com>
2022-10-10 14:28:11 +0000

    python kata - took 7 mins

---
+++ python/d10_10_2022/unit_tests.py
@@ -0,0 +1,28 @@
+from random import sample
+import unittest
+from binary_search import do_binary_search
+from k_largest import do_k_largest
+
+
+class BinarySearchTestCase(unittest.TestCase):
+    def test_do_binary_search(self):
+        arr = sample(range(0, 100), 100)
+        arr.sort()
+        for val in arr:
+            res = do_binary_search(arr, val)
+            self.assertGreaterEqual(res, 0)
+            self.assertEqual(arr[res], val)
+
+
+class KLargestTestCase(unittest.TestCase):
+    def test_do_k_largest(self):
+        arr = sample(range(0, 100), 100)
+        k = 5
+        res = do_k_largest(arr, k)
+        arr.sort(reverse=True)
+        for num in arr[:k]:
+            self.assertIn(num, res)
+
+
+if __name__=='__main__':
+    unittest.main()
