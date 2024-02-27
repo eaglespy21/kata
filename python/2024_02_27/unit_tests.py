ce6ccbb1e2a18ab781cd8ba1fe156e1abeb65f87
Saurabh Hindlekar <saurabhhindlekar@hotmail.com>
2024-02-27 08:22:09 -0800

    random sample

--- python/2024_02_27/unit_tests.py
+++ python/2024_02_27/unit_tests.py
@@ -6,10 +6,10 @@ from binary_search import do_bs
 class TestBinarySearch(unittest.TestCase):
 
     def test_binary_search(self):
-        n = 10
-        arr = [random.randrange(0, 1000) for _ in range(n)]
+        n = 10000
+        arr = random.sample(range(100000), n)
         arr.sort()
-        print(arr)
+        #print(arr)
         for i, num in enumerate(arr):
             ans = do_bs(arr, num)
             self.assertEqual(i, ans, " " + str(num))
