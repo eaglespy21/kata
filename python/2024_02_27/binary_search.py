fa46017bdf922dfbd38cff891dee0bfe2592f6aa
Saurabh Hindlekar <saurabhhindlekar@hotmail.com>
2024-02-27 08:31:50 -0800

    recursive version

--- python/2024_02_27/binary_search.py
+++ python/2024_02_27/binary_search.py
@@ -10,3 +10,15 @@ def do_bs(arr, k):
         else:
             left = mid + 1
     return -1
+
+
+def do_bs_r(arr, left, right, k):
+    if left > right:
+        return -1
+    mid = (left + right) // 2
+    if arr[mid] == k:
+        return mid
+    elif arr[mid] > k:
+        return do_bs_r(arr, left, mid - 1, k)
+    else:
+        return do_bs_r(arr, mid + 1, right, k)
--- python/2024_02_27/unit_tests.py
+++ python/2024_02_27/unit_tests.py
@@ -1,6 +1,6 @@
 import unittest
 import random
-from binary_search import do_bs
+from binary_search import do_bs, do_bs_r
 
 
 class TestBinarySearch(unittest.TestCase):
@@ -14,6 +14,14 @@ class TestBinarySearch(unittest.TestCase):
             ans = do_bs(arr, num)
             self.assertEqual(i, ans, " " + str(num))
 
+    def test_binary_search_r(self):
+        n = 10000
+        arr = random.sample(range(100000), n)
+        arr.sort()
+        #print(arr)
+        for i, num in enumerate(arr):
+            ans = do_bs_r(arr, 0, len(arr), num)
+            self.assertEqual(i, ans, " " + str(num))
 
 if __name__ == '__main__':
     unittest.main()
