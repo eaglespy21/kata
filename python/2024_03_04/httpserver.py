e27085831a55ae549181f9d53161685771855d4a
Saurabh Hindlekar <saurabhhindlekar@hotmail.com>
2024-03-04 21:23:40 -0800

    comments

--- python/2024_03_04/httpserver.py
+++ python/2024_03_04/httpserver.py
@@ -7,14 +7,15 @@ arr = []
 @hug.get('/remembrall_store')
 def rememberall_store(a: hug.types.delimited_list(',')):
     print(f'a is {a}')
-    a = [int(x) for x in a]
+    a = [int(x)
+         for x in a]  #Converting string to int has to happen, how to modularize this into a type?
     arr.extend(a)
     return f"the current arr is {arr}"
 
 
 @hug.get('/remembrall')
 def remembrall(num: hug.types.number):
-    arr.sort()
+    arr.sort()  #TODO sorting every time is expensive
     ans = do_bs(arr, num)
     if ans >= 0:
         return "Yes I have seen that number before"
